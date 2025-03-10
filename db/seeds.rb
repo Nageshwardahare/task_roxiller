f7ecac565cd8368bdd8fcf8a62fbd28266ba20c8
Nageshwardahare <nageshwardahare7@gmail.com>
2025-03-10 17:53:16 +0530

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+admin_role = Role.create!(name: "Admin")
+candidate_role = Role.create!(name: "Candidate")
+
+admin = User.create!(name: "Admin User", email: "admin@example.com", password: "password", role: admin_role)
+
+candidate1 = User.create!( name: "Candidate 1", email: "candidate1@example.com", password: "123456", role: candidate_role)
+
+candidate2 = User.create!( name: "Candidate 2", email: "candidate2@example.com", password: "123456", role: candidate_role
+)
+
+JobApplication.create!(
+  [
+    { user: candidate1 },
+    { user: candidate1 },
+    { user: candidate2 },
+    { user: candidate2 }
+  ]
+)
